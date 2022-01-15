95262c6b00ab9c13981a4b5017d8ccd75c6321dd
shashir22 <shashirgowdas67@gmail.com>
2022-01-15 20:24:14 +0530

    Added:Selection problems using if and else

---
+++ SelectionProblems/month.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+read -p " Enter Date:-" date
+read -p " Enter Month:-" Month
+
+if (( ($Month <= 6 && $month >= 3) && (($day >= 20 && $date <= 20) && ($ddate<31)) ))
+then
+        echo $Month $date "True";
+else
+
+        echo "False";
+fi
+© 2022 GitHub, Inc.
